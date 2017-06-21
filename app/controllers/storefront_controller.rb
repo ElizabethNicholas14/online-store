f5bc6ca4bfd15e7956a4ccff2ca5ecd015ef901a
ElizabethNicholas14 <elizabethnicholas14@gmail.com>
2017-06-21 10:33:07 -0500

    *add modal style to stylesheet *add ability to shop by category *change navbar by adding shop dropdown with dynamic categories and all products *update storefront controller to handle product filtering *update storefront index view to show current category

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,13 @@
 class StorefrontController < ApplicationController
   def index
-    @products = Product.all
+    if params[:cat_id ]
+    #I only want to see the stuff in that category
+      @category = Category.find(params[:cat_id])
+      #@products = Product.where(category_id: params[:cat_id])
+      @products = @category.products
+    else
+      @products = Product.all
+    end
   end
 
   def about
