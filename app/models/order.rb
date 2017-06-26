d3bb58cb7cd5fd607551ed20ec0ba8f296aa1577
ElizabethNicholas14 <elizabethnicholas14@gmail.com>
2017-06-26 09:03:31 -0500

    add orders table to database *set up relationships in models

---
+++ app/models/order.rb
@@ -0,0 +1,22 @@
+class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+  #sets up a direct relationship between the order and the line item - one has to exist with the other. If you destroy the order, it will destroy the line items
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  address    :text
+#  pay_type   :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_orders_on_user_id  (user_id)
+#
